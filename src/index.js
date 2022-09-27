ed1fa638282e8d946571c62a3298cbf2cc65fbef
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-28 00:44:46 +0530

    Wrapped ChakraProvider and Router around the App component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const MainApp = () => (
+  <Router>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Router>
+);
+
+root.render(<MainApp />);
