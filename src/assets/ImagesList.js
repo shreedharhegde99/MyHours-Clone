9e5a4516b93fb95ada1b8ed6cace37bd750edeb4
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-28 19:53:38 +0530

    Added image links required for Home page

--- src/assets/ImagesList.js
+++ src/assets/ImagesList.js
@@ -2,6 +2,34 @@ let imageList = {
   logo: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg",
   footerLogo:
     "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc9236f9ab08c7bdf83fe75_Logo-300px.png",
+  homePageHeading:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd337b26121202b5bfb96d7_OriginalSize(1)-p-1080.png",
+  capterra:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9399ba39d2583a755_Capterra-4-7.png",
+  G2logo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9b4eb5ca178cc55df_g2-4-7.png",
+  microsoftLogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/600008d9487a040f81a5d142_MS-Gold.png",
+  typeFoxlogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079c0b6e21682628f281_typefox%201.png",
+  KPMGLogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079cd7ceecebfc3f5519_kpmg_logo%201.png",
+  ImpartnerLogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079dfd768757fcf2ea64_logo-Impartner-1.png",
+  SemaphoreLogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079d3f7265588b6d1dbe_semaphore-logo.png",
+  AuroraLogo:
+    "	https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079d09b818dd84138753_aurora-logo-horiz-white%201.png",
+  ManpowerLogo:
+    "	https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/6000079d14844ef7226bb10a_logo_heading.png",
+  homePageCards: {
+    coOrdinate:
+      "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd7199052d14ec5e2a6c813_HP-1.png",
+    track:
+      "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd71991bd83c222217cac17_HP-2.png",
+    create:
+      "	https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd71991a6df42a49f7e6d5c_HP-3.png",
+  },
 };
 
 export { imageList };
