17f6a0ed2190eb9c769ee8741ed90dbc8d935854
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-28 12:08:04 +0530

    Created Footer component

--- src/assets/ImagesList.js
+++ src/assets/ImagesList.js
@@ -1,5 +1,7 @@
 let imageList = {
   logo: "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg",
+  footerLogo:
+    "https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fc9236f9ab08c7bdf83fe75_Logo-300px.png",
 };
 
 export { imageList };
