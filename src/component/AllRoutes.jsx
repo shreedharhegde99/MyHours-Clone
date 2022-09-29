f3d8370361f0fc6aa5f03948886d580e13389f3f
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-29 14:18:28 +0530

    Added route for pages completed

--- src/component/AllRoutes.jsx
+++ src/component/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Home, HowItWorks, UseCase } from "../pages";
 
 export function AllRoutes() {
   return (
     <Fragment>
       <Routes>
-        <Route path="/how-it-works" />
-        <Route path="/use-cases" />
+        <Route path="/" element={<Home />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/use-cases" element={<UseCase />} />
         <Route path="pricing" />
         <Route path="/support" />
         <Route path="/signin" />
