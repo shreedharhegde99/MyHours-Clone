f647f5caabe85c687373a2638b53cb0ea9c0f669
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-29 23:22:53 +0530

    Added route to pricing and support page

--- src/component/AllRoutes.jsx
+++ src/component/AllRoutes.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, HowItWorks, UseCase } from "../pages";
+import { Pricing } from "../pages/Pricing";
+import { Support } from "../pages/Support";
 
 export function AllRoutes() {
   return (
@@ -9,8 +11,8 @@ export function AllRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
         <Route path="/use-cases" element={<UseCase />} />
-        <Route path="pricing" />
-        <Route path="/support" />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/support" element={<Support />} />
         <Route path="/signin" />
         <Route path="/signup" />
       </Routes>
