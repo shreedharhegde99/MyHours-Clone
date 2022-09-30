efcc511df75d3a4ee6c6d4e878d3388e60bf418a
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-30 22:18:36 +0530

    Added link to signup page

--- src/component/GetStartedButton.jsx
+++ src/component/GetStartedButton.jsx
@@ -1,18 +1,21 @@
 import { Button, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export function GetStartedButton() {
   return (
-    <Flex justify={{ sm: "center" }} py="6" px="2">
-      <Button
-        w={{ base: "full", sm: "max-content" }}
-        height="14"
-        colorScheme="blue"
-        px={{ sm: "8" }}
-        fontSize="xl"
-        fontWeight="medium"
-      >
-        Get Started - It's Free
-      </Button>
+    <Flex justify={{ base: "center", sm: "center" }} py="6" px="2">
+      <Link to="/signup">
+        <Button
+          w={{ base: "full", sm: "max-content" }}
+          height="14"
+          colorScheme="blue"
+          px={{ base: "8" }}
+          fontSize="xl"
+          fontWeight="medium"
+        >
+          Get Started - It's Free
+        </Button>
+      </Link>
     </Flex>
   );
 }
