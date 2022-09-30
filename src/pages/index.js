512a654e1aeea8d97b0ed4a370cc25c9440d00c8
Shreedhar Hegde <snhkelemane@gmail.com>
2022-09-30 22:21:00 +0530

    Updated export for newly created pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,7 @@ export { Home } from "./Home";
 export { UseCase } from "./UseCase";
 export { HowItWorks } from "./HowItWorks";
 export { MobileApp } from "./MobileApp";
+export { Signin } from "./Signin";
+export { Support } from "./Support";
+export { Pricing } from "./Pricing";
+export { Signup } from "./Signup";
